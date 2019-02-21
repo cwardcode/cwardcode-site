b5dfa64458cb175c78d6a43d1235a4c4d3a4ead0
Chris Ward <chris.dev.ward@gmail.com>
2019-02-20 20:27:03 -0500

    Fix unit tests and add test-ci script

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,18 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { MockComponent } from 'ng-mocks';
+import { Terminal, TerminalModule } from 'primeng/terminal';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        TerminalModule
       ],
       declarations: [
         AppComponent
@@ -14,22 +20,13 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'cwardcode'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('cwardcode');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to cwardcode!');
+  });
+
+  it('should create the app', () => {
+        expect(component).toBeTruthy();
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
 import { TerminalService } from 'primeng/components/terminal/terminalservice';
-//import { Subscription } from 'rxjs/Subscription';
 import { Subscription } from 'rxjs';
 import { Terminal } from 'primeng/terminal';
 @Component({
@@ -14,13 +13,14 @@ export class AppComponent {
   @ViewChild('term') private terminalPrompt: Terminal;
   subscription: Subscription;
   promptText = 'cwardcode $';
-  welcomeMessage = 'Welcome to CWardCode. My new site is under construction. Until it is finished, play around and have fun! Type help for more information.'
+  // tslint:disable-next-line:max-line-length
+  welcomeMessage = 'Welcome to CWardCode. My new site is under construction. Until it is finished, play around and have fun! Type help for more information.';
   constructor(private terminalService: TerminalService) {
     this.terminalService.commandHandler.subscribe(cmd => {
       let response;
-      switch(cmd.toLowerCase()) {
+      switch (cmd.toLowerCase()) {
 
-        case 'whoami': {          
+        case 'whoami': {
           response = 'user@cwardcode.com';
           break;
         }
@@ -28,6 +28,7 @@ export class AppComponent {
         case 'login': {
           // TODO use actual library
           response = 'Redirecting to login page..';
+          // tslint:disable-next-line:max-line-length
           window.location.replace('https://auth.cwardcode.com/login?redirect_uri=https%3A%2F%2Fcwardcode.com&response_type=token&client_id=3e4nmhs5bkjkpc734silkdhd3f&state=TQz4g2Lm1yklfmUbtZlCeN0ptdIQOzL5&scope=openid%20profile%20email');
           break;
         }
@@ -39,8 +40,7 @@ export class AppComponent {
 
         case 'clear': {
           const commands = this.terminalPrompt.el.nativeElement.children[0];
-          console.log(commands.childNodes)
-          debugger;
+          console.log(commands.childNodes);
         /*  for(let i = 1; commands.childElementCount.length > 1; i++){
             console.log(commands.children[i]);
             //console.log(this.terminalPrompt.el.nativeElement.children.length)
@@ -64,8 +64,8 @@ export class AppComponent {
     });
   }
 
-  ngOnDestroy() {
-    if(this.subscription) {
+  OnDestroy() {
+    if (this.subscription) {
       this.subscription.unsubscribe();
     }
   }
